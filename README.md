c38fb5782f811100ebe958b45b3d6b0cc07d0b25
IgorGuz0206 <guzovatyi1993dp@gmail.com>
2021-07-08 16:37:23 +0300

    -

---
+++ README.md
@@ -0,0 +1,5 @@
+# grid____template
+# grid____template
+# grid____template
+# gridtemplate
+# grid
---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+// $(document).ready(function () {
+
+//   $('.header-navbar__hamburger').on('click', function () {
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').toggleClass('active');
+//   });
+
+// });
+
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('active');
+
+		// $('.header-navbar__list').toggleClass('header-navbar__list--active');
+		$('.header-navbar__list').stop(true, true).slideToggle(500);
+	});
+});
